ea5cff9fb1d0171d4e1d0885857cc49c4ce241f7
shainap-2005 <shainaparyanii@gmail.com>
2025-10-04 17:41:11 -0700

    Delete env.py

--- env.py
+++
@@ -1,7 +0,0 @@
-
-
-MONGODB_URI=mongodb+srv://climateUser:dW1BvjAuZFAJFbEt@cluster0.ux87zjb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-DB_NAME=climate_experiment
-COLL_NAME=runs
-PORT=3000
-
